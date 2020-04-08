8ac47b766a3c2318bf1a6b6f50718e5a105eb2d0
Kerem Dlkmn <15542014@firat.edu.tr>
2020-04-08 13:35:32 +0300

    SLPFunctions.py
    Code editing

--- basic-slp-algorithm/SLPFunctions.py
+++ basic-slp-algorithm/SLPFunctions.py
@@ -9,7 +9,6 @@ def reductionWeight(weight_1,weight_2,learning_coefficient,attribute_1,attribute
     return newWeightValue_1, newWeightValue_2
 
 def calculateThresholdValue(orange_net_value,apple_net_value,threshold_value):
-
     if(orange_net_value > threshold_value and apple_net_value > threshold_value):
         return True
     else:
