6b970faaadc7b23522023621699287b6e35e0ad0
Kerem Dlkmn <15542014@firat.edu.tr>
2020-04-08 13:34:42 +0300

    SLP Algorithm
    Basic level SLP Algorithm Code

---
+++ basic-slp-algorithm/SLPFunctions.py
@@ -0,0 +1,16 @@
+def calculateNETValue(attribute_1,attribute_2,weight_1,weight_2):
+    valueNET = (attribute_1*weight_1) + (attribute_2 * weight_2)
+    return valueNET
+
+def reductionWeight(weight_1,weight_2,learning_coefficient,attribute_1,attribute_2):
+    newWeightValue_1 = weight_1 - (learning_coefficient * attribute_1)
+    newWeightValue_2 = weight_2 - (learning_coefficient * attribute_2)
+
+    return newWeightValue_1, newWeightValue_2
+
+def calculateThresholdValue(orange_net_value,apple_net_value,threshold_value):
+
+    if(orange_net_value > threshold_value and apple_net_value > threshold_value):
+        return True
+    else:
+        return False
---
+++ basic-slp-algorithm/main.py
@@ -0,0 +1,59 @@
+## import SLPFunctions
+import SLPFunctions as slp
+
+## Magic Number -> Orange Attributes and Orange Class ##
+orange_attribute_1 = 1
+orange_attribute_2 = 0
+orange_class       = 1
+
+## Magic Number -> Apple Attributes and Apple Class ##
+apple_attribute_1 = 0
+apple_attribute_2 = 1
+apple_class       = 0
+
+## Magic Number -> Weight, Threshold Value ve Learning Coefficient ##
+weight_value_1  = 1
+weight_value_2  = 2
+threshold_value = -1
+learning_coefficient = 0.5
+
+## Iteration Value ##
+iteration_value = 0
+
+while(True):
+    print("------------------------------------")
+    iteration_value += 1
+    print(iteration_value, ". Iteration")
+    print("------------------------------------")
+
+    OrangeNET = slp.calculateNETValue(orange_attribute_1,orange_attribute_2,weight_value_1,weight_value_2)
+    AppleNET = slp.calculateNETValue(apple_attribute_1,apple_attribute_2,weight_value_2,weight_value_2)
+
+    print("NET Orange: ",OrangeNET)
+    print("NET Apple: ",AppleNET)
+    print("------------------------------------")
+
+    if(OrangeNET == orange_class):
+        if(slp.calculateThresholdValue(OrangeNET,AppleNET,threshold_value)):
+            print("Çıktı : Orange")
+            if(AppleNET == apple_class):
+                if(slp.calculateThresholdValue(OrangeNET,AppleNET,threshold_value)):
+                    print("Çıktı : Apple")
+                    break
+                else:
+                    weight_value_1, weight_value_2 = slp.reductionWeight(weight_value_1, weight_value_2,
+                                                                        learning_coefficient, apple_attribute_1,
+                                                                        apple_attribute_2)
+            else:
+                weight_value_1, weight_value_2 = slp.reductionWeight(weight_value_1, weight_value_2,
+                                                                    learning_coefficient, apple_attribute_1, apple_attribute_2)
+        else:
+            weight_value_1, weight_value_2 = slp.reductionWeight(weight_value_1, weight_value_2, learning_coefficient,apple_attribute_1, apple_attribute_2)
+    else:
+        weight_value_1, weight_value_2 = slp.reductionWeight(weight_value_1, weight_value_2, learning_coefficient,orange_attribute_1, orange_attribute_2)
+
+print("------------------------")
+print("Class Prediction")
+print("Orange Class : ", OrangeNET)
+print("Apple Class  : ", AppleNET)
+print("Itration Value: ", iteration_value)
